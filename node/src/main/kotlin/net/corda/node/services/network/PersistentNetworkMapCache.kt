4ddd402302d49c8b934e79576034d6c2f0a9b19d
Denis Rekalov <denis.rekalov@r3.com>
2020-09-28 10:16:13 +0100

    CORDA-3761: Auto-correction of rotated notary identity inside TransactionBuilder

--- node/src/main/kotlin/net/corda/node/services/network/PersistentNetworkMapCache.kt
+++ node/src/main/kotlin/net/corda/node/services/network/PersistentNetworkMapCache.kt
@@ -57,7 +57,13 @@ open class PersistentNetworkMapCache(cacheFactory: NamedCacheFactory,
     @Volatile
     private lateinit var notaries: List<NotaryInfo>
 
+    @Volatile
+    private lateinit var rotatedNotaries: Set<CordaX500Name>
+
+    // Notary whitelist may contain multiple identities with the same X.500 name after certificate rotation.
+    // Exclude duplicated entries, which are not present in the network map.
     override val notaryIdentities: List<Party> get() = notaries.map { it.identity }
+            .filterNot { it.name in rotatedNotaries && it != getPeerCertificateByLegalName(it.name)?.party }
 
     override val allNodeHashes: List<SecureHash>
         get() {
@@ -73,7 +79,7 @@ open class PersistentNetworkMapCache(cacheFactory: NamedCacheFactory,
         }
 
     fun start(notaries: List<NotaryInfo>) {
-        this.notaries = notaries
+        onNewNotaryList(notaries)
     }
 
     override fun getNodeByLegalIdentity(party: AbstractParty): NodeInfo? {
@@ -96,6 +102,8 @@ open class PersistentNetworkMapCache(cacheFactory: NamedCacheFactory,
             session.createQuery(query).resultList.singleOrNull()?.toNodeInfo()
         }
     }
+
+    override fun isNotary(party: Party): Boolean = notaries.any { it.identity == party }
 
     override fun isValidatingNotary(party: Party): Boolean = notaries.any { it.validating && it.identity == party }
 
@@ -412,5 +420,6 @@ open class PersistentNetworkMapCache(cacheFactory: NamedCacheFactory,
 
     override fun onNewNotaryList(notaries: List<NotaryInfo>) {
         this.notaries = notaries
+        this.rotatedNotaries = notaries.groupBy { it.identity.name }.filter { it.value.size > 1 }.keys
     }
 }
